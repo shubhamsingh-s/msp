d5bef698b8abfa6845b77a97123e1fd6e8ee908d
shubhamsingh-s <shubham.s.45665@gmail.com>
2026-06-04 23:34:30 +0530

    Fix Vercel 404 routing by separating static CDN files and serverless API handlers

--- api/index.js
+++ api/index.js
@@ -10,25 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Route incoming API calls to their respective handlers
-app.get('/api/products', require('../api-handlers/products'));
-app.get('/api/product', require('../api-handlers/product'));
-app.post('/api/add-product', require('../api-handlers/add-product'));
-app.put('/api/update-product', require('../api-handlers/update-product'));
-app.delete('/api/delete-product', require('../api-handlers/delete-product'));
+app.get('/api/products', require('./products'));
+app.get('/api/product', require('./product'));
+app.post('/api/add-product', require('./add-product'));
+app.put('/api/update-product', require('./update-product'));
+app.delete('/api/delete-product', require('./delete-product'));
 
-app.post('/api/add-enquiry', require('../api-handlers/add-enquiry'));
-app.get('/api/enquiries', require('../api-handlers/enquiries'));
-app.put('/api/enquiries', require('../api-handlers/enquiries'));
-app.delete('/api/enquiries', require('../api-handlers/enquiries'));
+app.post('/api/add-enquiry', require('./add-enquiry'));
+app.get('/api/enquiries', require('./enquiries'));
+app.put('/api/enquiries', require('./enquiries'));
+app.delete('/api/enquiries', require('./enquiries'));
 
-app.get('/api/categories', require('../api-handlers/categories'));
-app.post('/api/categories', require('../api-handlers/categories'));
-app.put('/api/categories', require('../api-handlers/categories'));
-app.delete('/api/categories', require('../api-handlers/categories'));
+app.get('/api/categories', require('./categories'));
+app.post('/api/categories', require('./categories'));
+app.put('/api/categories', require('./categories'));
+app.delete('/api/categories', require('./categories'));
 
-app.post('/api/login', require('../api-handlers/login'));
-app.post('/api/logout', require('../api-handlers/logout'));
-app.get('/api/check-auth', require('../api-handlers/check-auth'));
+app.post('/api/login', require('./login'));
+app.post('/api/logout', require('./logout'));
+app.get('/api/check-auth', require('./check-auth'));
 
 module.exports = app;
 module.exports.config = {
