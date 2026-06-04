69c932c99c71ff0de10897d516c18aa15b6066bf
shubhamsingh-s <shubham.s.45665@gmail.com>
2026-06-05 00:02:00 +0530

    Refactor API structure to single serverless function and enable Zero-Config static serving

--- api/index.js
+++ api/index.js
@@ -4,31 +4,40 @@ require('dotenv').config();
 
 const app = express();
 
+// Restore original req.url from Vercel's rewrite headers
+app.use((req, res, next) => {
+  const originalPath = req.headers['x-vercel-forwarded-path'] || req.headers['x-forwarded-path'];
+  if (originalPath) {
+    req.url = originalPath;
+  }
+  next();
+});
+
 // Parse request bodies and cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Route incoming API calls to their respective handlers
-app.get('/api/products', require('./products'));
-app.get('/api/product', require('./product'));
-app.post('/api/add-product', require('./add-product'));
-app.put('/api/update-product', require('./update-product'));
-app.delete('/api/delete-product', require('./delete-product'));
+app.get('/api/products', require('../api-handlers/products'));
+app.get('/api/product', require('../api-handlers/product'));
+app.post('/api/add-product', require('../api-handlers/add-product'));
+app.put('/api/update-product', require('../api-handlers/update-product'));
+app.delete('/api/delete-product', require('../api-handlers/delete-product'));
 
-app.post('/api/add-enquiry', require('./add-enquiry'));
-app.get('/api/enquiries', require('./enquiries'));
-app.put('/api/enquiries', require('./enquiries'));
-app.delete('/api/enquiries', require('./enquiries'));
+app.post('/api/add-enquiry', require('../api-handlers/add-enquiry'));
+app.get('/api/enquiries', require('../api-handlers/enquiries'));
+app.put('/api/enquiries', require('../api-handlers/enquiries'));
+app.delete('/api/enquiries', require('../api-handlers/enquiries'));
 
-app.get('/api/categories', require('./categories'));
-app.post('/api/categories', require('./categories'));
-app.put('/api/categories', require('./categories'));
-app.delete('/api/categories', require('./categories'));
+app.get('/api/categories', require('../api-handlers/categories'));
+app.post('/api/categories', require('../api-handlers/categories'));
+app.put('/api/categories', require('../api-handlers/categories'));
+app.delete('/api/categories', require('../api-handlers/categories'));
 
-app.post('/api/login', require('./login'));
-app.post('/api/logout', require('./logout'));
-app.get('/api/check-auth', require('./check-auth'));
+app.post('/api/login', require('../api-handlers/login'));
+app.post('/api/logout', require('../api-handlers/logout'));
+app.get('/api/check-auth', require('../api-handlers/check-auth'));
 
 module.exports = app;
 module.exports.config = {
